e3b8d7d7199b18365b2991ea4785832aba157369
Abmaellf <abmael.ninha@gmail.com>
2025-05-12 22:53:08 -0400

    feat(Formatando valores)

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -2,6 +2,7 @@ import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 import { SummaryCard, SummaryContainer } from "./styles";
 import { useContext } from "react";
 import { TransactionContext } from "../../contexts/TransactionsContext";
+import { priceFormatter } from "../../utils/formatter";
 
 export function Summary() {
 
@@ -34,7 +35,7 @@ export function Summary() {
                     <ArrowCircleUp size={32} color="#00b37e" />
                 </header>
 
-                <strong> { summary.income } </strong>
+                <strong> { priceFormatter.format(summary.income )} </strong>
             </SummaryCard>
             <SummaryCard>
                 <header>
@@ -42,7 +43,7 @@ export function Summary() {
                     <ArrowCircleDown size={32} color="#f75a68" />
                 </header>
 
-                <strong>  { summary.outcomo } </strong>
+                <strong>  { priceFormatter.format(summary.outcomo) } </strong>
             </SummaryCard>
             <SummaryCard variant="green">
                 <header>
@@ -50,7 +51,7 @@ export function Summary() {
                     <CurrencyDollar size={32} color="#fff" />
                 </header>
 
-                <strong>  {summary.total } </strong>
+                <strong>  {priceFormatter.format(summary.total) }</strong>
             </SummaryCard>
 
         </SummaryContainer>
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -4,6 +4,7 @@ import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 import { TransactionContext } from "../../contexts/TransactionsContext";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 
 
 
@@ -24,13 +25,14 @@ export function Transactions() {
                                     <td  width="60%" > {transaction.description}</td>
                                     
                                         <td> 
-                                            <PriceHighlight variant="income"> 
-                                                {transaction.price}
+                                            <PriceHighlight variant={transaction.type}> 
+                                                {transaction.type === 'outcome' && '- '}
+                                                {priceFormatter.format(transaction.price)}
                                             </PriceHighlight> 
                                         </td>
                                     
                                     <td> {transaction.category} </td>
-                                    <td> {transaction.createdAt} </td>
+                                    <td> {dateFormatter.format(new Date(transaction.createdAt))} </td>
                                 </tr>
                             )
                         })}
